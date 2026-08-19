7a27dcedc51be7d17c84848e0cc7063e37c13f6f
mmajeed7864 <hmajeed04@gmail.com>
2026-08-19 12:16:03 -0400

    Refresh FitCoach PWA cache for distinct v0.3.3 pages

--- fitcoach-founder-test/sw.js
+++ fitcoach-founder-test/sw.js
@@ -1,12 +1,13 @@
-const CACHE = "fitcoach-symbio-v0320";
+const CACHE = "fitcoach-symbio-v0330";
 const ASSETS = [
   "./",
-  "./index.html?v=0320",
+  "./index.html?v=0330",
   "./v031-style-01.css?v=0310",
   "./v031-style-02.css?v=0310",
   "./v031-style-03.css?v=0310",
   "./v031-style-04.css?v=0310",
   "./v031-style-05.css?v=0310",
+  "./v033-pages.css?v=0330",
   "./v031-part-01.js?v=0310",
   "./v031-part-02.js?v=0310",
   "./v031-part-03.js?v=0310",
@@ -21,38 +22,26 @@ const ASSETS = [
   "./v031-part-12.js?v=0310",
   "./v031-part-13.js?v=0310",
   "./v032-ai-voice.js?v=0320",
-  "./manifest.webmanifest?v=0320",
-  "./assets/icon-symbio.svg?v=0320"
+  "./v033-pages.js?v=0330",
+  "./manifest.webmanifest?v=0330",
+  "./assets/icon-symbio.svg?v=0330"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   const url = new URL(event.request.url);
   if (url.origin !== self.location.origin) return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request).then(hit => hit || caches.match("./")))
-  );
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(hit => hit || caches.match("./"))));
 });
